b84652cbaf4f610029e6e8667818b547bb76fe3e
ChrisGelles-AtTheMuseum <139920213+ChrisGelles-AtTheMuseum@users.noreply.github.com>
2023-11-22 14:50:51 -0500

    Initial holiday card build.

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Lottie A (Plays Once)
+    var animationA = lottie.loadAnimation({
+        container: document.getElementById('lottieA'),
+        renderer: 'svg',
+        loop: false,
+        autoplay: true,
+        path: 'top-snowflakes.json' // Update this path
+    });
+
+    // Lottie B (First Frame Only)
+    var animationB = lottie.loadAnimation({
+        container: document.getElementById('lottieB'),
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        path: 'snowglobe-text-01.json' // Update this path
+    });
+    animationB.goToAndStop(0, true); // Go to the first frame and stop
+
+    // Lottie C (Loops Continuously)
+    var animationC = lottie.loadAnimation({
+        container: document.getElementById('lottieC'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'snowglobe-loop-01.json' // Update this path
+    });
+});
